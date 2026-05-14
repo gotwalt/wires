6fa3d7950256de643400a482b2f9d2d5f920052c
Aaron Gotwalt <gotwalt@users.noreply.github.com>
2026-05-14 12:49:33 -0400

    wires-core: reserved-type registry and mode enforcement

--- crates/wires-core/src/lib.rs
+++ crates/wires-core/src/lib.rs
@@ -2,6 +2,7 @@ pub mod cap;
 pub mod chain;
 pub mod content;
 pub mod error;
+pub mod reserved;
 pub mod sig;
 pub mod wire;
 
@@ -9,5 +10,6 @@ pub use cap::{Capability, CapIdRepr, Right, glob_matches};
 pub use chain::{next_prev_hash, verify_chain_link};
 pub use content::CanonicalContent;
 pub use error::{CoreError, Result};
+pub use reserved::{check_kind_matches, is_reserved, required_mode_for, RequiredMode};
 pub use sig::{sign_envelope, verify_envelope};
 pub use wire::{CapId, MessageHash, MessageKind, Pubkey, Signature, TopicId, WireMessage};
